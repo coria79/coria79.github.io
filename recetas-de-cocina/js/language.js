93a7fdc483f01f5fe00fa931b090b0b0afbd4bf1
coria79 <83871145+coria79@users.noreply.github.com>
2024-01-05 21:36:46 -0300

    Add files via upload

--- recetas-de-cocina/js/language.js
+++ recetas-de-cocina/js/language.js
@@ -1,32 +1,33 @@
-let selectedLanguage = 'es';
-
-function changeLanguage(language) {
+function changeLanguage(language, event) {
     // Obtener el nombre de la página actual
     let currentPage = window.location.pathname;
 
-    if (language === 'en') {
-        // Redirigir a la página en inglés correspondiente
-        window.location.href = currentPage.replace('.html', '-eng.html');
-    } else {
-        // Redirigir a la versión en español
-        window.location.href = currentPage.replace('-eng.html', '.html');
+    switch (language) {
+        case 'en':
+            if (!currentPage.includes('-eng.html')) {
+                // Redirigir a la página en inglés correspondiente solo si aún no está en inglés
+                window.location.href = currentPage.replace(/\.html$/, '-eng.html');
+                // Establecer el idioma seleccionado a inglés
+                selectedLanguage = 'en';
+                // Prevenir el comportamiento predeterminado del enlace
+                event.preventDefault();
+            }
+            break;
+        case 'es':
+            if (currentPage.includes('-eng.html')) {
+                // Redirigir a la página en español correspondiente solo si está en inglés
+                window.location.href = currentPage.replace(/\-eng.html$/, '.html');
+                // Establecer el idioma seleccionado a español
+                selectedLanguage = 'es';
+                // Prevenir el comportamiento predeterminado del enlace
+                event.preventDefault();
+            }
+            break;
+        default:
+            // En caso de un idioma no reconocido, no hacer nada
+            return false;
     }
-
-    // Establecer el idioma seleccionado
-    selectedLanguage = language;
 
     // Retornar false para prevenir el comportamiento predeterminado del enlace
     return false;
 }
-
-// Cargar la página según el idioma seleccionado.
-document.addEventListener('DOMContentLoaded', function () {
-    if (selectedLanguage === 'en') {
-        let currentPage = window.location.pathname;
-        if (currentPage.includes('-eng.html')) {
-            // Si ya estamos en la versión en inglés, no hace falta redireccionar
-            return;
-        }
-        window.location.href = currentPage.replace('.html', '-eng.html');
-    }
-});
